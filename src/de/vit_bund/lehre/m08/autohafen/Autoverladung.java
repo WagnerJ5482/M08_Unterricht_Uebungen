1e75696ad034875a32dbf41bb164b582a8dd329a
Jens.Wagner <Jens.Wagner@vit17-101.vit.intern>
2019-05-20 09:55:15 +0200

    Uebung02 initial

--- src/de/vit_bund/lehre/m08/autohafen/Autoverladung.java
+++ src/de/vit_bund/lehre/m08/autohafen/Autoverladung.java
@@ -1,21 +1,5 @@
 package de.vit_bund.lehre.m08.autohafen;
 
 public class Autoverladung {
-	private String zielort;
-	private int zaehlerAutos;
-	private int anzahlAutos;
-	private Auto[] inhalt;
-	
-	public void addAuto(Auto auto) {
-		
-	}
-	
-	public void printAutoliste() {
-		
-	}
-	
-	public void berechnePreisMax() {
-		
-	}
-	
+
 }
