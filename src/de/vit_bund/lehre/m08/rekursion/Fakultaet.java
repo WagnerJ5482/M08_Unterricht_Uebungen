680f5fb110059e565403549e6d8ff39fd7ed00a7
Jens.Wagner <Jens.Wagner@vit17-101.vit.intern>
2019-05-29 12:33:25 +0200

    Iterativ mit while / for (BigInteger) und richtig interativ unter long

--- src/de/vit_bund/lehre/m08/rekursion/Fakultaet.java
+++ src/de/vit_bund/lehre/m08/rekursion/Fakultaet.java
@@ -1,14 +1,27 @@
 package de.vit_bund.lehre.m08.rekursion;
 
+import java.math.BigInteger;
+
 public class Fakultaet {
 
 	public static void main(String[] args) {
 		System.out.println(iterativWhile(5));
-
+		System.out.println(iterativLong(20));
+		System.out.println(iterativBigInteger(40));
 	}
 	
-	public static long interativ(int n) {
-		return 0;
+	public static long iterativLong(long n) {
+		if (n == 0) return 1;
+		else return n*iterativLong(n-1);
+	}
+	
+	public static BigInteger iterativBigInteger(long n) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= n; i++)
+        {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
 	}
 	
 	
